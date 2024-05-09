466da59d0af4aa6ee0df17160a46523fe6892f79
Aleksey Pardaev <lpardaev607@gmail.com>
2024-05-09 18:32:34 +0300

    4

--- task2-singleton-pattern/src/main/java/com/mjc/stage2/ThreadSafeSingleton.java
+++ task2-singleton-pattern/src/main/java/com/mjc/stage2/ThreadSafeSingleton.java
@@ -1,11 +1,11 @@
 package com.mjc.stage2;
 
 public class ThreadSafeSingleton {
-    private ThreadSafeSingleton instance;
+    private static ThreadSafeSingleton instance;
 
     private ThreadSafeSingleton(){}
 
-    public synchronized ThreadSafeSingleton getInstance() {
+    public static synchronized ThreadSafeSingleton getInstance() {
 
         if(instance == null) {
             instance = new ThreadSafeSingleton();
